ef3070f4c47a2710c82699ae5c5a224f0b96b828
Oliver Günther <mail@oliverguenther.de>
2025-09-24 11:24:52 +0200

    Remove unused code

--- app/controllers/enterprise_tokens_controller.rb
+++ app/controllers/enterprise_tokens_controller.rb
@@ -39,11 +39,7 @@ class EnterpriseTokensController < ApplicationController
   before_action :find_token, only: %i[destroy destroy_dialog]
   before_action :check_trial_status, only: [:index]
 
-  def index
-    # TODO: delete next line. @current_token is used in the old angular thingy.
-    # It should not be needed anymore then.
-    @current_token = EnterpriseToken.active_tokens.first
-  end
+  def index; end
 
   def new
     respond_with_dialog Admin::EnterpriseTokens::CreateDialogComponent.new(EnterpriseToken.new)
@@ -63,7 +59,6 @@ class EnterpriseTokensController < ApplicationController
       # restore the old token
       if saved_encoded_token
         @token.encoded_token = saved_encoded_token
-        @current_token = @token || EnterpriseToken.new
       end
       respond_to do |format|
         format.html { render action: :index, status: :unprocessable_entity }
