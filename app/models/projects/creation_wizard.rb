6e2835549a8951e43dacc9f4424941f33522e25b
Dombi Attila <83396+dombesz@users.noreply.github.com>
2025-11-24 13:57:01 +0200

    [#69350] Change title to "New project initiation request" when creating from a template
    https://community.openproject.org/work_packages/69350

--- app/models/projects/creation_wizard.rb
+++ app/models/projects/creation_wizard.rb
@@ -44,10 +44,15 @@ module Projects::CreationWizard
     store_attribute :settings, :project_creation_wizard_work_package_comment, :string
     store_attribute :settings, :project_creation_wizard_artifact_export_type, :string, default: "attachment"
     store_attribute :settings, :project_creation_wizard_artifact_export_storage, :string
+
+    # The store_attribute default cannot be used here, because the default is not returned
+    # when the JSON defintion is present but it's nil.
+    def project_creation_wizard_artifact_name
+      super.presence || "project_creation_wizard"
+    end
   end
 
   def project_creation_wizard_name
-    name = project_creation_wizard_artifact_name || "project_creation_wizard"
-    I18n.t("settings.project_initiation_request.name.options.#{name}")
+    I18n.t("settings.project_initiation_request.name.options.#{project_creation_wizard_artifact_name}")
   end
 end
