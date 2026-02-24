9e0226e592c9506e90165a102346a01f8e6b8f45
Ivan Kuchin <i.kuchin@openproject.com>
2026-02-24 16:56:19 +0100

    test pdf export with custom fields by admin

--- spec/models/projects/exports/pdf_spec.rb
+++ spec/models/projects/exports/pdf_spec.rb
@@ -101,26 +101,30 @@ RSpec.describe Projects::Exports::PDF do
 
     context "without view_project_attributes permission" do
       let(:permissions) { super() - %i[view_project_attributes] }
-
-      it "does not include custom field values in the export" do
-        expected_document = [
+      let(:expected_document) do
+        [
           *expected_cover_page,
           project.name,
           "ID", project.id.to_s,
           "1/1", export_time_formatted, query.name
         ].join(" ")
+      end
+
+      it "does not include custom field values in the export" do
         expect(subject).to eq expected_document
       end
     end
 
     context "without export_projects permission" do
       let(:permissions) { %i(view_projects) }
-
-      it "does not include the project in the export" do
-        expected_document = [
+      let(:expected_document) do
+        [
           *expected_cover_page,
           "1/1", export_time_formatted, query.name
         ].join(" ")
+      end
+
+      it "does not include the project in the export" do
         expect(subject).to eq expected_document
       end
     end
@@ -140,7 +144,7 @@ RSpec.describe Projects::Exports::PDF do
       when "text"
         "Some  long  text"
       when "string", "list"
-        "Some small text"
+        custom_field.admin_only? ? "hidden" : "Some small text"
       when "date"
         format_date(Time.zone.today)
       when "link"
@@ -149,14 +153,36 @@ RSpec.describe Projects::Exports::PDF do
     end
 
     context "with view_project_attributes permission" do
-      it "includes custom field values in the export" do
-        expected_document = [
+      let(:expected_document) do
+        [
           *expected_cover_page,
           project.name,
           "ID", project.id.to_s,
-          *global_project_custom_fields.sort_by(&:name).flat_map { |column| [column.name, get_test_column_value(column)] },
+          *global_project_custom_fields.sort_by(&:name).map { |column| [column.name, get_test_column_value(column)] },
           "1/1", export_time_formatted, query.name
         ].join(" ")
+      end
+
+      it "includes custom field values in the export" do
+        expect(subject).to eq expected_document
+      end
+    end
+
+    context "with admin permission" do
+      let(:current_user) { build_stubbed(:admin) }
+      let(:expected_document) do
+        custom_fields = global_project_custom_fields - [not_used_string_cf]
+
+        [
+          *expected_cover_page,
+          project.name,
+          "ID", project.id.to_s,
+          *custom_fields.sort_by(&:name).map { |column| [column.name, get_test_column_value(column)] },
+          "1/1", export_time_formatted, query.name
+        ].join(" ")
+      end
+
+      it "includes custom field values in the export" do
         expect(subject).to eq expected_document
       end
     end
