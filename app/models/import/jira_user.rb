62675edf8038e70e39cb6af22e3e8fac17fbe118
as-op <a.sandorf@openproject.com>
2026-03-03 09:14:40 +0100

    refactor display name handling to improve firstname and lastname extraction

--- app/models/import/jira_user.rb
+++ app/models/import/jira_user.rb
@@ -40,9 +40,7 @@ module Import
     end
 
     def to_op_attributes
-      parts = payload["displayName"].split
-      firstname = parts.length > 1 ? parts[0..-2].join(" ") : parts[0]
-      lastname = parts.length > 1 ? parts[-1] : parts[0]
+      firstname, lastname = split_display_name(payload["displayName"])
       {
         login: payload["name"],
         password: SecureRandom.uuid,
@@ -58,6 +56,13 @@ module Import
       User.where(login: op_attributes[:login]).or(
         User.where(mail: op_attributes[:mail])
       )
+    end
+
+    private
+
+    def split_display_name(display_name)
+      parts = display_name.split
+      parts.length > 1 ? [parts[0..-2].join(" "), parts[-1]] : [parts[0], parts[0]]
     end
   end
 end
