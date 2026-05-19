780aa63668213ddbdfc7cd9a8b8f0da1f856e5c6
Eric Schubert <e.schubert@openproject.com>
2026-05-19 15:02:04 +0200

    [#73354] fix component test

--- modules/wikis/spec/components/wikis/relation_page_links_component_spec.rb
+++ modules/wikis/spec/components/wikis/relation_page_links_component_spec.rb
@@ -87,12 +87,14 @@ RSpec.describe Wikis::RelationPageLinksComponent, type: :component do
   end
 
   context "when the user has a token and there are page links" do
+    let(:page_link) { create(:relation_wiki_page_link, linkable: work_package, provider:) }
     let(:page_info) do
       Wikis::Adapters::Results::PageInfo.new(
         identifier: "MyPage",
         provider:,
         title: "My Wiki Page",
-        href: "https://wiki.example.com/MyPage"
+        href: "https://wiki.example.com/MyPage",
+        page_link:
       )
     end
 
