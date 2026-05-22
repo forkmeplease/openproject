e9f7c7dd7cd4c99331fdfb164ad885d0d95faf8d
ulferts <jens.ulferts@googlemail.com>
2026-05-22 18:06:02 +0200

    centralize loading of backlog same as sprints

--- modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
+++ modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
@@ -104,10 +104,14 @@ module Backlogs
     end
 
     def backlog_component
-      inbox_work_packages = WorkPackage.backlogs_inbox_for(project: @project)
       buckets = BacklogBucket.for_project(@project)
 
-      Backlogs::BacklogComponent.new(inbox_work_packages:, buckets:, project: @project)
+      work_packages_by_backlog_id = WorkPackage
+                                       .in_backlog_for(project: @project)
+                                       .includes(:type, :status, :assigned_to, :priority, :parent)
+                                       .group_by(&:backlog_bucket_id)
+
+      Backlogs::BacklogComponent.new(buckets:, work_packages_by_backlog_id:, project: @project)
     end
 
     def load_work_package
@@ -125,7 +129,7 @@ module Backlogs
       elsif @work_package.backlog_bucket_id?
         @work_packages.merge(@work_package.backlog_bucket.displayed_work_packages)
       else
-        @work_packages.merge(WorkPackage.backlogs_inbox_for(project: @project))
+        @work_packages.merge(WorkPackage.in_backlog_for(project: @project).where(backlog_bucket_id: nil))
       end
     end
   end
--- modules/backlogs/app/models/work_packages/scopes/in_backlog_for.rb
+++ modules/backlogs/app/models/work_packages/scopes/in_backlog_for.rb
@@ -28,17 +28,16 @@
 # See COPYRIGHT and LICENSE files for more details.
 # ++
 
-module WorkPackages::Scopes::BacklogsInboxFor
+module WorkPackages::Scopes::InBacklogFor
   extend ActiveSupport::Concern
 
   class_methods do
-    def backlogs_inbox_for(project:)
+    def in_backlog_for(project:)
       WorkPackage
         .visible
-        .where(project:, sprint_id: nil, backlog_bucket_id: nil)
+        .where(project:, sprint_id: nil)
         .without_excluded_type
         .without_status_considered_closed
-        .includes(:assigned_to, :priority, :parent)
         .order_by_position
         .order(WorkPackage.arel_table[:id].asc)
     end
