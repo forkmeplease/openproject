9dfcd421edeedfb4aff7e7ae2d5768bce45b573b
Dombi Attila <83396+dombesz@users.noreply.github.com>
2026-06-03 13:52:35 +0300

    Fix brittle have_turbo_stream_template method
    Using an asser_select will give a meaningful error message when the
    template element cannot be found.

--- modules/backlogs/spec/controllers/backlogs/work_packages_controller_spec.rb
+++ modules/backlogs/spec/controllers/backlogs/work_packages_controller_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Backlogs::WorkPackagesController do
   # Gets the html content of the template of the first turbo-stream with the
   # given action.
   def turbo_stream_template(action:)
-    Nokogiri("<response>#{response.body}</response>").css("turbo-stream[action=#{action}] template").first.inner_html
+    assert_select("turbo-stream[action=#{action}] template").first.inner_html
   end
 
   shared_let(:type_feature) { create(:type_feature) }
