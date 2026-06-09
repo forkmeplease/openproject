a7c3702edbf3dd22d7ffd4f40a5748056dc3ada2
Jan Sandbrink <j.sandbrink@openproject.com>
2026-06-09 16:46:38 +0200

    Wire-up page search using URL to dialogs
    This allows to use the URL of a wiki page during selection
    of a page to link or the parent of a page to be created.

---
+++ modules/wikis/app/services/wikis/page_search_service.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Wikis
+  class PageSearchService
+    include Dry::Monads[:result]
+
+    attr_reader :provider, :user
+
+    def initialize(provider:, user:)
+      @provider = provider
+      @user = user
+    end
+
+    def search_pages(query)
+      return Success([]) if query.blank?
+
+      search_by_url(query).or do |error|
+        return Failure(error) if error.code != :not_found
+
+        search_by_query(query)
+      end
+    end
+
+    private
+
+    def search_by_url(query)
+      return Failure(Adapters::Results::Error.new(source: self.class, code: :not_found)) unless url?(query)
+
+      Adapters::Input::PageInfoForUrl.build(url: query).bind do |input_data|
+        provider.auth_strategy_for(user).bind do |auth_strategy|
+          provider.resolve("queries.page_info_for_url").call(input_data:, auth_strategy:).fmap { [it] }
+        end
+      end
+    end
+
+    def search_by_query(query)
+      Adapters::Input::SearchPages.build(query:).bind do |input_data|
+        provider.auth_strategy_for(user).bind do |auth_strategy|
+          provider.resolve("queries.search_pages").call(input_data:, auth_strategy:)
+        end
+      end
+    end
+
+    def url?(string)
+      uri = URI.parse(string)
+
+      %w[http https].include?(uri.scheme)
+    rescue URI::InvalidURIError
+      false
+    end
+  end
+end
